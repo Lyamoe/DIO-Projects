ceb74a1f049bb97dc4dd8532d4a3ead8b758fb0c
Lyamoe <karinesanper@gmail.com>
2024-09-07 12:18:18 -0300

    Organização de pacotes

--- Java/aula1/src/exercicios/ContaTerminal.java
+++ Java/aula1/src/exercicios/ContaTerminal.java
@@ -1,5 +1,5 @@
-<<<<<<< HEAD
 package exercicios;
+
 import java.util.Scanner;
 
 public class ContaTerminal {
@@ -22,28 +22,3 @@ public class ContaTerminal {
         scanner.close();
     }
 }
-=======
-package exercicios;
-import java.util.Scanner;
-
-public class ContaTerminal {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Digite o número da conta: ");
-        int numero = scanner.nextInt();
-        System.out.print("Digite o número da agencia que está: ");
-        String agencia = scanner.nextLine();
-        System.out.print("Digite o seu nome: ");
-        String nomeCliente = scanner.nextLine();
-        System.out.print("Digite o valor do 1° depósito: ");
-        double saldo = scanner.nextInt();
-
-        System.out.println("Olá " + nomeCliente +
-        ", obrigado por criar uma conta em nosso banco! Sua agÊncia é " + agencia +
-        ", conta " + numero + " e seu saldo " + saldo + " já está disponível.");
-
-        scanner.close();
-    }
-}
->>>>>>> a08ed086f8fe493d68fcdbd466828880bfc9f97e
--- Java/aula1/src/exercicios/ControleDeFluxo.java
+++ Java/aula1/src/exercicios/ControleDeFluxo.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package exercicios;
 import java.util.Scanner;
 
@@ -35,42 +34,4 @@ public class ControleDeFluxo {
             System.out.println(parametroUm + i);
         }
     }
-=======
-package exercicios;
-import java.util.Scanner;
-
-public class ControleDeFluxo {
-	public static void main(String[] args) {
-		Scanner terminal = new Scanner(System.in);
-		System.out.println("Digite o primeiro parâmetro");
-		int parametroUm = terminal.nextInt();
-		System.out.println("Digite o segundo parâmetro");
-		int parametroDois = terminal.nextInt();
-        terminal.close();
-		
-		try {
-			//chamando o método contendo a lógica de contagem
-			contar(parametroUm, parametroDois);
-		}catch (Exception e) {
-			System.out.println("O segundo parâmetro deve ser maior que o primeiro");
-		}
-		
-	}
-
-    public static class ParametrosInvalidosException extends Exception {
-        public ParametrosInvalidosException(String message) {
-            super(message);
-        }
-    }
-
-    public static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
-        if (parametroUm > parametroDois) {
-            throw new ParametrosInvalidosException("parametroUm não pode ser maior que parametroDois");
-        }
-        int contagem = parametroDois - parametroUm;
-        for (int i = 0; i <= contagem; i++) {
-            System.out.println(parametroUm + i);
-        }
-    }
->>>>>>> a08ed086f8fe493d68fcdbd466828880bfc9f97e
 }
