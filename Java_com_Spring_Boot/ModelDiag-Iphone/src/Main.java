4e33961902d91706d337a6ddb21d77276fc7dcea
Lyamoe <karinesanper@gmail.com>
2025-08-25 14:09:49 -0300

    Correção de erros no código java

--- Java_com_Spring_Boot/ModelDiag-Iphone/src/Main.java
+++ Java_com_Spring_Boot/ModelDiag-Iphone/src/Main.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import celular.Iphone;
 
 public class Main {
@@ -10,17 +9,4 @@ public class Main {
         iphone.atender();
         iphone.iniciarCorreioVoz();
     }
-=======
-import celular.Iphone;
-
-public class Main {
-    public static void main(String[] args) throws InterruptedException {
-        Iphone iphone = new Iphone();
-        
-        //? testando funções
-        iphone.ligar();
-        iphone.atender();
-        iphone.iniciarCorreioVoz();
-    }
->>>>>>> a08ed086f8fe493d68fcdbd466828880bfc9f97e
 }
--- Java_com_Spring_Boot/ModelDiag-Iphone/src/celular/funcionalidades/Navegador.java
+++ Java_com_Spring_Boot/ModelDiag-Iphone/src/celular/funcionalidades/Navegador.java
@@ -1,13 +1,4 @@
-<<<<<<< HEAD
 package celular.funcionalidades;
-
 public class Navegador {
     // Métodos: exibirPagina(String url), adicionarNovaAba(), atualizarPagina()
 }
-=======
-package celular.funcionalidades;
-
-public class Navegador {
-    // Métodos: exibirPagina(String url), adicionarNovaAba(), atualizarPagina()
-}
->>>>>>> a08ed086f8fe493d68fcdbd466828880bfc9f97e
